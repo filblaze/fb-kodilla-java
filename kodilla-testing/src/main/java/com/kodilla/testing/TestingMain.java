b29f73cb52a7294f2edb24d0b895a538d6fc9c0e
Filip Błażewicz <filipblazewicz@gmail.com>
2025-03-30 22:21:29 +0200

    6.2 Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        //Test user
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("user test OK");
+        } else {
+            System.out.println("user test FAIL");
+        }
+
+        //Test calculator
+        Calculator calculator = new Calculator();
+
+        int Sum = calculator.addAToB(1, 2);
+        int Diff = calculator.subtractBFromA(10, 1);
+
+        if(Sum==3 && Diff==9){
+            System.out.println("calculator test OK");
+        } else {
+            System.out.println("calculator test FAIL");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractBFromA(int a, int b) {
+        return a - b;
+    }
+}

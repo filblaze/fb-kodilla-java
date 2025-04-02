a6619f06a0c7497a7d231a6f062903623b2425a7
Filip Błażewicz <filipblazewicz@gmail.com>
2025-04-02 19:14:52 +0200

    6.3.1 Test klasy wybierającej parzyste liczby z kolekcji

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -27,9 +27,9 @@ public class CollectionTestSuite {
         Integer[] liczby = new Integer[]{};
         //When
         System.out.println("Testing empty list");
-        o.exterminate(Arrays.asList(liczby));
+        List<Integer> result = o.exterminate(Arrays.asList(liczby));
         //Then
-        Assertions.assertEquals(Collections.emptyList(), o.exterminate(Arrays.asList(liczby)));
+        Assertions.assertEquals(Collections.emptyList(), result);
     }
 
     @DisplayName("When an Integer number list is provided " +
@@ -41,9 +41,9 @@ public class CollectionTestSuite {
         Integer[] liczby = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         //When
         System.out.println("Testing normal list");
-        o.exterminate(Arrays.asList(liczby));
+        List<Integer> result = o.exterminate(Arrays.asList(liczby));
         //Then
-        Assertions.assertEquals(Arrays.asList(2, 4, 6, 8, 10), o.exterminate(Arrays.asList(liczby)));
+        Assertions.assertEquals(Arrays.asList(2, 4, 6, 8, 10), result);
     }
 
     @DisplayName("When an Integer number list without even numbers is provided " +
@@ -55,8 +55,8 @@ public class CollectionTestSuite {
         Integer[] liczby = new Integer[]{1, 3, 5, 7, 9};
         //When
         System.out.println("Testing normal list w/o even numbers");
-        o.exterminate(Arrays.asList(liczby));
+        List<Integer> result = o.exterminate(Arrays.asList(liczby));
         //Then
-        Assertions.assertEquals(Collections.emptyList(), o.exterminate(Arrays.asList(liczby)));
+        Assertions.assertEquals(Collections.emptyList(), result);
     }
 }
